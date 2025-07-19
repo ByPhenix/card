f8a9c5559f8cf5187409d57cf6b7ff2cd049c07b
ByPhenix <contact@byphenix.com>
2025-07-19 15:50:57 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,17 +1,20 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// Import des fonctions Firebase
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
-// Your web app's Firebase configuration
+// Configuration Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyA2-dpxks9OqC8b3k0UKCoRbVt7ykr5_Yw",
+  apiKey: "AIzaSyA2-dqkks90qCb8b3k0UKCoRbVt7ykr5_Yw",
   authDomain: "card-78b75.firebaseapp.com",
   projectId: "card-78b75",
-  storageBucket: "card-78b75.firebasestorage.app",
+  storageBucket: "card-78b75.appspot.com",
   messagingSenderId: "771776470223",
   appId: "1:771776470223:web:4aa180f8e64a7b8f0e2bba"
 };
 
-// Initialize Firebase
+// Initialiser Firebase
 const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+// Exporter la base de données pour l'utiliser ailleurs
+export { db };
